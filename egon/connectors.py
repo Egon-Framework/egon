ed084e751a50f37fd6dbb7278651489cd9d08566
Daniel <djperrefort@gmail.com>
2022-10-30 19:13:20 -0400

    Adds connection logic to the base connector class

--- egon/connectors.py
+++ egon/connectors.py
@@ -7,7 +7,7 @@ and ``Input`` objects are used to receive data.
 
 from __future__ import annotations
 
-from typing import Tuple
+from typing import Optional, Tuple
 
 
 class BaseConnector:
@@ -23,19 +23,32 @@ class BaseConnector:
             name: Optional name for the connector object
         """
 
+        # Identifying information for the instance
         self._id = hex(id(self))
         self.name = str(self._id) if name is None else name
 
+        # The parent node
+        self._node = None
+
+        # Other connector objects connected to this instance
+        self._connected_partners = []
+
     @property
-    def parent_node(self):
+    def parent_node(self) -> Optional:  # Todo: update this type hint
         """The parent node this connector is assigned to"""
+
+        return self._node
 
     @property
     def partners(self) -> Tuple:
         """Return a tuple of connectors that are connected to this instance"""
 
+        return tuple(self._connected_partners)
+
     def is_connected(self) -> bool:
         """Return whether the connector has any established connections"""
+
+        return bool(self._connected_partners)
 
     def __str__(self) -> str:
         """Return the name of the parent instance"""
