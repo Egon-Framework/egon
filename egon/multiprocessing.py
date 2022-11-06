f41d6b0abd595250f21d88a05f4d42b4012a34fe
Daniel <djperrefort@gmail.com>
2022-11-06 08:55:55 -0500

    Documentation pass

--- egon/multiprocessing.py
+++ egon/multiprocessing.py
@@ -6,31 +6,41 @@ import multiprocessing as mp
 
 
 class MultiprocessingEngine:
-    """A processes pool for executing callable objects in parallel"""
+    """A processes pool for executing callable objects in parallel
+
+    The ``MultiprocessingEngine`` class is much less general than the built-in
+    ``Pool`` object. For example, instances are tied to a single callable
+    object defined at init and cannot be dynamically mapped to other callables
+    (i.e., there are no `map`` or ``apply`` methods).
+    """
 
     def __init__(self, num_processes: int, target: callable) -> None:
         """Create a new engine instance for evaluating the given callable
 
         Args:
-            num_processes: The number of new processes to run in parallel
+            num_processes: The number of processes to run in parallel
             target: The callable object to be executed in parallel
         """
 
         self._target = target
         self._processes = []  # Collection of processes managed by the parent instance
-        self._states = mp.Manager().dict()  # Map process memory id to execution state
+        self._states = mp.Manager().dict()  # Map process memory id to process execution state
 
         self._locked = False
         self.set_num_processes(num_processes)
 
-    def _wrap_target(self) -> None:  # pragma: nocover - called from child process
+    def _wrap_target(self) -> None:  # pragma: nocover - this method called from child process
         """Wrapper method for calling the target function and updating process status"""
 
         self._target()
         self._states[id(mp.current_process())] = True
 
     def reset(self) -> None:
-        """Reset the engine instance so it can be reused"""
+        """Reset the engine instance so it can be reused
+
+        Raises:
+            RuntimeError: When resetting an engine before it has finished executing
+        """
 
         if not self.is_finished():
             raise RuntimeError('Some processes have not finished executing')
@@ -48,6 +58,10 @@ class MultiprocessingEngine:
 
         Args:
             num_processes: The number of processes to allocate
+
+        Raises:
+            RuntimeError: When modifying an engine that has already been started
+            ValueError: When the number of processes is invalid
         """
 
         if self._locked:
@@ -65,13 +79,13 @@ class MultiprocessingEngine:
         return all(self._states.values())
 
     def run(self) -> None:
-        """Start all processes asynchronously"""
+        """Start all processes and join them to the current process"""
 
         self.run_async()
         self.join()
 
     def run_async(self) -> None:
-        """Start all processes and join them to the current process"""
+        """Start all processes asynchronously"""
 
         self._locked = True
         for p in self._processes:
