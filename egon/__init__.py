9ae386de1e1e85c88d6a28fada42b35c6beebab6
Daniel <djperrefort@gmail.com>
2022-11-25 15:16:18 -0500

    Removes unused imports

--- egon/__init__.py
+++ egon/__init__.py
@@ -1,7 +1,3 @@
 """A Python parallelization framework for easily building powerful data analysis networks."""
 
-from .connectors import InputConnector, OutputConnector
-from .nodes import Node
-from .pipeline import Pipeline
-
 __version__ = '0.0.0'
